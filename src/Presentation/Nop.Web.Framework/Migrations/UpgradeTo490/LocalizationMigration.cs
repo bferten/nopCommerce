522861e0f1a38018f4cb77312735b7936b714a31
Alexey Anokhin <exile.developer@gmail.com>
2025-08-07 14:42:48 +0300

    7807 Improved accessibility for all frontend pages

--- src/Presentation/Nop.Web.Framework/Migrations/UpgradeTo490/LocalizationMigration.cs
+++ src/Presentation/Nop.Web.Framework/Migrations/UpgradeTo490/LocalizationMigration.cs
@@ -304,13 +304,13 @@ public class LocalizationMigration : MigrationBase
             ["Wishlist.NotFound"] = "Wishlist not found.",
             ["Wishlist.DeleteWishlist"] = "Delete wishlist",
             ["Wishlist.SelectWishlist"] = "Specify your wishlist",
-            ["Wishlist.MaximumNumberReached"] = "You cannot create more than {0} custom wishlists.", 
+            ["Wishlist.MaximumNumberReached"] = "You cannot create more than {0} custom wishlists.",
             ["Wishlist.NameRequired"] = "A wishlist name is required.",
             ["Products.ProductHasBeenAddedToTheCustomWishlist.Link"] = "The product has been added to your <a href=\"{0}\">{1}</a>",
             ["Products.ProductHasBeenAddedToTheWishlistAndMoved.Link"] = "The product has been added to your <a href=\"{0}\">wishlist</a>. Want to move it to a <a href=\"#\" onclick=\"{1}\">custom wishlist</a>?",
             ["Wishlist.MultipleWishlistNotForGuest"] = "The multiple wishlist functionality is only available to registered customers.",
             ["Wishlist.NotAllowMultipleWishlist"] = "Multiple wishlist functionality is disabled.",
- 
+
             //#7739
             ["Admin.System.Maintenance.DeleteThumbFiles.FilesCount"] = "Total files: {0}",
             ["Admin.System.Maintenance.DeleteThumbFiles"] = "Delete image thumbs",
@@ -318,6 +318,10 @@ public class LocalizationMigration : MigrationBase
             ["Admin.System.Maintenance.DeleteThumbFiles.Text"] = "Delete image thumbs from the thumbs directory. All files except the placeholder.txt file.",
             ["Admin.System.Maintenance.DeleteThumbFiles.IsNotSupported"] = "Delete image thumbs is not supported by current picture thumb service.",
             ["Admin.System.Maintenance.DeleteThumbFiles.Deleted"] = "All image thumbs were deleted.",
+
+            //7807
+            ["Forum.Post.Text"] = "Post Text",
+            ["Forum.TopicText"] = "Forum Text",
         }, languageId);
 
         #endregion
